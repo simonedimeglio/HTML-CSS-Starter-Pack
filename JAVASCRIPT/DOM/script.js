d9c437bc716f25dd01991ff6c2ffb53b0aa9ea50
simonedimeglio <simonedimeglio@icloud.com>
2024-05-08 14:52:26 +0200

    DOM init

---
+++ JAVASCRIPT/DOM/script.js
@@ -0,0 +1,18 @@
+// SELETTORI
+let elementoDaId = document.getElementById("element");
+// let elementoDallaClasse = document.getElementsByClassName("classe");
+// let elementoDaTag = document.getElementsByTagName("div");
+// let elementoDaQuery = document.querySelector("#id .classe");
+// let elentoDaQueryMultipla = document.querySelectorAll(".paperino");
+
+// Modifica delle classi
+// elementoDaId.classList.add("ciao");
+// elementoDaId.classList.remove("red");
+
+// Modifica dello stile
+// elementoDaId.style.backgroundColor = "black";
+
+elementoDaId.addEventListener("click", function () {
+  console.log("ciao");
+  elementoDaId.classList.toggle("red");
+});
